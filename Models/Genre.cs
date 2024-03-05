fba45b7394b7b13bbc9d9d071ab3b59709b717fa
Dylan Moore <dylankmoore@gmail.com>
2024-03-04 19:06:49 -0600

    set up models

---
+++ Models/Genre.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TunaPiano.Models
+{
+    public class Genre
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Description { get; set; }
+        public ICollection<Song> Songs { get; set; }
+
+    }
+}
